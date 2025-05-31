68a628ab0e0e703fb243e6d307a699a1a73b6e62
Pedro Tola <pedrotolamol@gmail.com>
2025-05-31 01:13:44 -0500

    Part 9 - Exercise 9.16

--- part9/courseinfo/src/App.tsx
+++ part9/courseinfo/src/App.tsx
@@ -1,12 +1,38 @@
 import type { JSX } from "react";
+import assertNever from "./helper";
 
-type CourseParts = {
+type CoursePartBase = {
   name: string,
   exerciseCount: number
 }
 
+interface CoursePartDescription extends CoursePartBase {
+  description: string
+}
+
+interface CoursePartBasic extends CoursePartDescription {
+  kind: "basic"
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number
+  kind: "group"
+}
+
+interface CoursePartBackground extends CoursePartDescription {
+  backgroundMaterial: string,
+  kind: "background"
+}
+
+interface CoursePartSpecial extends CoursePartDescription {
+  requirements: string[],
+  kind: "special"
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartSpecial
+
 type CourseProps = {
-  courseParts: CourseParts[]
+  courseParts: CoursePart[]
 }
 
 type HeaderProps = {
@@ -27,11 +53,46 @@ const Content = ({courseParts}: CourseProps): JSX.Element => {
   return(
     <div>
       {
-        courseParts.map((part, index)=> (
-          <p key={index}> 
-            {part.name} {part.exerciseCount}
-          </p>
-        ))
+        courseParts.map((part)=> {
+          switch (part.kind) {
+            case "basic":
+              return(
+                <div key={part.name}>
+                  <p>
+                    <strong>{part.name} {part.exerciseCount}</strong> <br />
+                    <em>{part.description}</em>
+                  </p>
+                </div>
+              );
+            case "group":
+              return(
+              <div key={part.name}>
+                <p>
+                  <strong>{part.name} {part.exerciseCount}</strong> <br />
+                  project exercises {part.groupProjectCount}
+                </p>  
+              </div>
+              );
+            case "background":
+              return(
+                <div key={part.name}>
+                  <p><strong>{part.name} {part.exerciseCount}</strong> <br />
+                  <em>{part.description}</em> <br />
+                  submit to {part.backgroundMaterial}</p>
+                </div>
+              );
+            case "special":
+              return(
+                <div key={part.name}>
+                  <p><strong>{part.name} {part.exerciseCount}</strong><br />
+                  <em>{part.description}</em><br />
+                  required skills: {part.requirements.join(', ')}</p>
+                </div>       
+              );
+            default:
+              return assertNever(part);
+          }
+        })
       }
     </div>
   )
@@ -45,22 +106,60 @@ const Header = (props: HeaderProps): JSX.Element => {
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group"
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"
+    },{
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special"
     }
-  ];
+  ]
 
   const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0);
+
+  courseParts.forEach(part => {
+    switch (part.kind) {
+      case "basic":
+        console.log(part.name, part.description, part.exerciseCount);
+        break;
+      case "group":
+        console.log(part.name, part.groupProjectCount, part.exerciseCount);
+        break;
+      case "background":
+        console.log(part.name, part.description, part.exerciseCount);
+        break;
+      case "special":
+        console.log(part.name, part.description, part.exerciseCount);
+        break;
+      default:
+        return assertNever(part);
+    }
+  });
 
   return (
     <div>
